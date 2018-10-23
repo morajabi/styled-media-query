f26a3a55c6c1fedc5e3b18b4e2c51088e783f23e
timhagn <mail@timhagn.com>
2018-10-23 13:15:59 +0200

    update .babelrc to .babelrc.js, inlude in .babelrc

---
+++ .babelrc.js
@@ -0,0 +1,10 @@
+const presets = [
+  [
+    "@babel/env",
+    {
+      "useBuiltIns": "entry"
+    },
+  ],
+];
+
+module.exports = { presets };
--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import uglify from 'rollup-plugin-uglify';
-import gzip from 'rollup-plugin-gzip';
 
 const prod = process.env.PRODUCTION;
 
