47238786887ca414b3a8c0a8bcaa8b79b89b319b
Tim Hagn <mail@timhagn.com>
2019-10-25 18:28:01 +0200

    remove superfluous default breakpoints for getSizeFromBreakpoint(), add .prettierrc.js, prettify tests

--- __tests__/convertors.test.js
+++ __tests__/convertors.test.js
@@ -1,62 +1,65 @@
-import { defaultBreakpoints } from "../src/index";
-import {pxToEm, pxToRem} from "../src/convertors";
+import { defaultBreakpoints } from '../src/index';
+import { pxToEm, pxToRem } from '../src/convertors';
 
-export const testBreakPoints = {
-  huge: "1440",
-  large: "1170",
-  medium: "768",
-  small: "450"
+export const itBreakPoints = {
+  huge: '1440',
+  large: '1170',
+  medium: '768',
+  small: '450',
 };
 
-test("pxToEm to equal defaultBreakpoints converted to em", () => {
-  expect(pxToEm(defaultBreakpoints, 16)).toEqual({
-    huge: "90em",
-    large: "73.125em",
-    medium: "48em",
-    small: "28.125em"
+describe(`pxToEm()`, () => {
+  it('pxToEm to equal defaultBreakpoints converted to em', () => {
+    expect(pxToEm(defaultBreakpoints, 16)).toEqual({
+      huge: '90em',
+      large: '73.125em',
+      medium: '48em',
+      small: '28.125em',
+    });
+  });
+
+  it('pxToEm without ratio to equal defaultBreakpoints converted to em', () => {
+    expect(pxToEm(defaultBreakpoints)).toEqual({
+      huge: '90em',
+      large: '73.125em',
+      medium: '48em',
+      small: '28.125em',
+    });
+  });
+
+  it('pxToEm itBreakPoints to equal itBreakPoints', () => {
+    expect(pxToEm(itBreakPoints, 16)).toEqual(itBreakPoints);
+  });
+
+  it('pxToEm with integer to match empty Object', () => {
+    expect(pxToRem(16, 16)).toMatchObject({});
   });
 });
 
-test("pxToEm without ratio to equal defaultBreakpoints converted to em", () => {
-  expect(pxToEm(defaultBreakpoints)).toEqual({
-    huge: "90em",
-    large: "73.125em",
-    medium: "48em",
-    small: "28.125em"
+describe(`pxToEm()`, () => {
+  it('pxToRem to equal defaultBreakpoints converted to rem', () => {
+    expect(pxToRem(defaultBreakpoints, 16)).toEqual({
+      huge: '90rem',
+      large: '73.125rem',
+      medium: '48rem',
+      small: '28.125rem',
+    });
+  });
+
+  it('pxToRem without ratio to equal defaultBreakpoints converted to rem', () => {
+    expect(pxToRem(defaultBreakpoints)).toEqual({
+      huge: '90rem',
+      large: '73.125rem',
+      medium: '48rem',
+      small: '28.125rem',
+    });
+  });
+
+  it('pxToRem itBreakPoints to equal itBreakPoints', () => {
+    expect(pxToRem(itBreakPoints, 16)).toEqual(itBreakPoints);
+  });
+
+  it('pxToRem {} to equal {}', () => {
+    expect(pxToRem({}, 16)).toEqual({});
   });
 });
-
-test("pxToEm testBreakPoints to equal testBreakPoints", () => {
-  expect(pxToEm(testBreakPoints, 16)).toEqual(testBreakPoints);
-});
-
-test("pxToRem to equal defaultBreakpoints converted to rem", () => {
-  expect(pxToRem(defaultBreakpoints, 16)).toEqual({
-    huge: "90rem",
-    large: "73.125rem",
-    medium: "48rem",
-    small: "28.125rem"
-  });
-});
-
-test("pxToRem without ratio to equal defaultBreakpoints converted to rem", () => {
-  expect(pxToRem(defaultBreakpoints)).toEqual({
-    huge: "90rem",
-    large: "73.125rem",
-    medium: "48rem",
-    small: "28.125rem"
-  });
-});
-
-test("pxToRem testBreakPoints to equal testBreakPoints", () => {
-  expect(pxToRem(testBreakPoints, 16)).toEqual(testBreakPoints);
-});
-
-test("pxToRem {} to equal {}", () => {
-  expect(pxToRem({}, 16)).toEqual({});
-});
-
-test("pxToEm with integer to match empty Object", () => {
-  expect(pxToRem(16, 16)).toMatchObject({});
-});
-
--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,25 +1,25 @@
 import 'jest-styled-components';
-import { generateMedia, defaultBreakpoints } from '../src/index'
+import { generateMedia, defaultBreakpoints } from '../src/index';
 import React from 'react';
-import renderer from 'react-test-renderer'
-import styled from 'styled-components'
-import { css } from 'styled-components'
+import renderer from 'react-test-renderer';
+import styled from 'styled-components';
+import { css } from 'styled-components';
 
 const generatedDefaultMedia = generateMedia(defaultBreakpoints);
 
 const Box = styled.div`
   background: black;
 
-  ${generatedDefaultMedia.lessThan("medium")`
+  ${generatedDefaultMedia.lessThan('medium')`
     background: red;
   `}
 
-  ${generatedDefaultMedia.between("medium", "large")`
+  ${generatedDefaultMedia.between('medium', 'large')`
     /* screen width is between 768px (medium) and 1170px (large) */
     background: green;
   `}
 
-  ${generatedDefaultMedia.greaterThan("large")`
+  ${generatedDefaultMedia.greaterThan('large')`
     /* screen width is greater than 1170px (large) */
     background: blue;
   `}
@@ -33,115 +33,141 @@ const Box = styled.div`
   `}
 `;
 
-
-
-test('expect <Box /> to have matching media style rules', () => {
-  let tree = renderer.create(<Box />).toJSON();
-  expect(tree).toMatchSnapshot();
-  expect(tree).toHaveStyleRule('background', 'red', {
-    'media': "(max-width:768px)"
-  });
-  expect(tree).toHaveStyleRule('background', 'green', {
-    'media': "(min-width:768px) and (max-width:1170px)"
-  });
-  expect(tree).toHaveStyleRule('background', 'blue', {
-    'media': "(min-width:1170px)"
-  });
-  expect(tree).toHaveStyleRule('background', 'white', {
-    'media': "(max-width:1170px)"
-  });
-  expect(tree).toHaveStyleRule('background', 'yellow', {
-    'media': "(min-width:450px)"
+describe(`generatedDefaultMedia()`, () => {
+  it('expect <Box /> to have matching media style rules', () => {
+    let tree = renderer.create(<Box/>).toJSON();
+    expect(tree).toMatchSnapshot();
+    expect(tree).toHaveStyleRule('background', 'red', {
+      media: '(max-width:768px)',
+    });
+    expect(tree).toHaveStyleRule('background', 'green', {
+      media: '(min-width:768px) and (max-width:1170px)',
+    });
+    expect(tree).toHaveStyleRule('background', 'blue', {
+      media: '(min-width:1170px)',
+    });
+    expect(tree).toHaveStyleRule('background', 'white', {
+      media: '(max-width:1170px)',
+    });
+    expect(tree).toHaveStyleRule('background', 'yellow', {
+      media: '(min-width:450px)',
+    });
   });
 });
 
-
-
-test('expect lessThan("medium") to create Array', () => {
-  let tree = renderer.create(css`${generatedDefaultMedia.lessThan("medium")`
+describe(`lessThan()`, () => {
+  it('expect lessThan("medium") to create Array', () => {
+    let tree = renderer
+        .create(
+            css`
+        ${generatedDefaultMedia.lessThan('medium')`
     background: red
-  `}`).toJSON();
-  let testArray = [
-    "\n    @media (max-width: ",
-    "768px",
-    ") {\n      ",
-    "\n    background: red\n  ",
-    "\n    }\n  "
-  ];
-  expect(tree).toEqual(expect.arrayContaining(testArray));
+  `}
+      `
+        )
+        .toJSON();
+    let itArray = [
+      '\n    @media (max-width: ',
+      '768px',
+      ') {\n      ',
+      '\n    background: red\n  ',
+      '\n    }\n  ',
+    ];
+    expect(tree).toEqual(expect.arrayContaining(itArray));
+  });
 });
 
-
-
-test('expect between("medium", "large") to create Array', () => {
-  let tree = renderer.create(css`${generatedDefaultMedia.between("medium", "large")`
+describe(`between()`, () => {
+  it('expect between("medium", "large") to create Array', () => {
+    let tree = renderer
+        .create(
+            css`
+        ${generatedDefaultMedia.between('medium', 'large')`
     background: green;
-  `}`).toJSON();
-  let testArray = [
-    "\n    @media (min-width: ",
-    "768px",
-    ") and\n      (max-width: ",
-    "1170px",
-    ") {\n      ",
-    "\n    background: green;\n  ",
-    "\n    }\n  "
-  ];
-  expect(tree).toEqual(expect.arrayContaining(testArray));
+  `}
+      `
+        )
+        .toJSON();
+    let itArray = [
+      '\n    @media (min-width: ',
+      '768px',
+      ') and\n      (max-width: ',
+      '1170px',
+      ') {\n      ',
+      '\n    background: green;\n  ',
+      '\n    }\n  ',
+    ];
+    expect(tree).toEqual(expect.arrayContaining(itArray));
+  });
 });
 
-
-
-test('expect greaterThan("large") to create Array', () => {
-  let tree = renderer.create(css`${generatedDefaultMedia.greaterThan("large")`
+describe(`greaterThan()`, () => {
+  it('expect greaterThan("large") to create Array', () => {
+    let tree = renderer
+        .create(
+            css`
+        ${generatedDefaultMedia.greaterThan('large')`
     background: blue;
-  `}`).toJSON();
-  let testArray = [
-    "\n    @media (min-width: ",
-    "1170px",
-    ") {\n      ",
-    "\n    background: blue;\n  ",
-    "\n    }\n  "
-  ];
-  expect(tree).toEqual(expect.arrayContaining(testArray));
-});
+  `}
+      `
+        )
+        .toJSON();
+    let itArray = [
+      '\n    @media (min-width: ',
+      '1170px',
+      ') {\n      ',
+      '\n    background: blue;\n  ',
+      '\n    }\n  ',
+    ];
+    expect(tree).toEqual(expect.arrayContaining(itArray));
+  });
 
-
-
-test('expect greaterThan(320) to create Array', () => {
-  let tree = renderer.create(css`${generatedDefaultMedia.greaterThan('320')`
+  it('expect greaterThan(320) to create Array', () => {
+    let tree = renderer
+        .create(
+            css`
+        ${generatedDefaultMedia.greaterThan('320')`
     background: blue;
-  `}`).toJSON();
-  let testArray = [
-    "\n    @media (min-width: ",
-    "320",
-    ") {\n      ",
-    "\n    background: blue;\n  ",
-    "\n    }\n  "
-  ];
-  expect(tree).toEqual(expect.arrayContaining(testArray));
-});
+  `}
+      `
+        )
+        .toJSON();
+    let itArray = [
+      '\n    @media (min-width: ',
+      '320',
+      ') {\n      ',
+      '\n    background: blue;\n  ',
+      '\n    }\n  ',
+    ];
+    expect(tree).toEqual(expect.arrayContaining(itArray));
+  });
 
-
-
-test('expect greaterThan() to create Array', () => {
-  let tree = renderer.create(css`${generatedDefaultMedia.greaterThan('')`
+  it('expect greaterThan() to create Array', () => {
+    let tree = renderer
+        .create(
+            css`
+        ${generatedDefaultMedia.greaterThan('')`
     background: blue;
-  `}`).toJSON();
-  let testArray = [
-    "\n    @media (min-width: ",
-    "0",
-    ") {\n      ",
-    "\n    background: blue;\n  ",
-    "\n    }\n  "
-  ];
-  expect(tree).toEqual(expect.arrayContaining(testArray));
+  `}
+      `
+        )
+        .toJSON();
+    let itArray = [
+      '\n    @media (min-width: ',
+      '0',
+      ') {\n      ',
+      '\n    background: blue;\n  ',
+      '\n    }\n  ',
+    ];
+    expect(tree).toEqual(expect.arrayContaining(itArray));
+  });
 });
 
-
-
-test('expect defaultBreakpoints to equal sizes', () => {
-  expect(defaultBreakpoints.small).toEqual('450px');
-  expect(defaultBreakpoints.medium).toEqual('768px');
-  expect(defaultBreakpoints.large).toEqual('1170px');
-  expect(defaultBreakpoints.huge).toEqual('1440px');
+describe(`defaultBreakpoints`, () => {
+  it('expect defaultBreakpoints to equal sizes', () => {
+    expect(defaultBreakpoints.small).toEqual('450px');
+    expect(defaultBreakpoints.medium).toEqual('768px');
+    expect(defaultBreakpoints.large).toEqual('1170px');
+    expect(defaultBreakpoints.huge).toEqual('1440px');
+  });
 });
